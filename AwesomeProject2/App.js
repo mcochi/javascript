a8d53a2ad04115c75fded8d10cdd6b83e2b9664c
mcochi <coro.dri@hotmail.com>
2020-03-01 21:09:52 +0100

    Incluyo proyecto AwesomeProject2 referente a una aplicación para incremento de la difusión. Versión Estable 0.0 - TABS: DGRII, THINKTIC, Convocatorias Europeas

---
+++ AwesomeProject2/App.js
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import { Text, View, Image, StatusBarIOS } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Container, Header, Thumbnail, Left, Body, Right, Button, Icon, Title, Segment, Content , Card, CardItem} from 'native-base';
+import { WebView } from 'react-native-webview';
+
+function HomeScreen({ navigation }) {
+  //Estaría bien incluir los posibles estudios que realizamos, linkados a nuestra página web
+  return (
+    <Container>
+          <Header>
+          <Left/>
+          <Body>
+            <Title>DGRII</Title>
+          </Body>
+          <Right />
+      </Header>
+      <Card style={{heigth: 100}}>
+            <CardItem>
+              <Left>
+                <Thumbnail source={require('./SRI.png')} />
+                <Body>
+                  <Text>Sistema Riojano de Innovación</Text>
+                  <Text note>DGRII</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Body>
+                  <Text>
+                  En esta app encontrarás las últimas noticias acerca de noticias sobre vigilancia de mercados, 
+                  noticias sobre innovación Tecnológica y no Tecnológica, 
+                  últimas acciones llevadas a cabo desde la dirección
+                  </Text>
+              </Body>
+            </CardItem>
+          </Card>
+      <WebView
+        source={{ html: 
+        '<a class="twitter-timeline" href="https://twitter.com/riojainnovacion?ref_src=twsrc%5Etfw">Tweets by thinktic</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
+       }}
+      />
+    </Container>
+    
+  );
+}
+
+function ThinkTICScreen() {
+  // Aquí falta subir los cursos ofrecidos por el ThinkTIC
+  return (
+    <Container>
+          <Header>
+          <Left/>
+          <Body>
+            <Title>DGRII</Title>
+          </Body>
+          <Right />
+      </Header>
+      <Card style={{heigth: 100}}>
+            <CardItem>
+              <Left>
+                <Thumbnail source={require('./ThinkTic.jpg')} />
+                <Body>
+                  <Text>ThinkTIC</Text>
+                  <Text note>DGRII</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Body>
+                  <Text>
+                    Aquí encontrarás los últimos cursos ofertados en el ThinkTIC, así como las noticias más interesantes que hemos encontrado:
+                  </Text>
+              </Body>
+            </CardItem>
+          </Card>
+      <WebView
+        source={{ html: 
+        '<a class="twitter-timeline" href="https://twitter.com/thinktic?ref_src=twsrc%5Etfw">Tweets by thinktic</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
+       }}
+      />
+    </Container>
+  );
+}
+
+function EuropaScreen() {
+  /*Falta linkarlo con las convocatorias Europeas abiertas, que puedo generarlas ampliando mi api y 
+  listando todos los que han sido introducidos durante la última semana*/
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Europa Calls!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="ThinkTIC" component={ThinkTICScreen} />
+      <Tab.Screen name="Convocatorias Europeas" component={EuropaScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
